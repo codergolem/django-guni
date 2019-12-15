f6ce73f637d231252ea90585a7818ed9790499c1
codergolem <mariocast@posteo.de>
2019-12-15 16:32:23 -0600

    Working with NGINX

--- mariopage/mariopage/wsgi.py
+++ mariopage/mariopage/wsgi.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+sys.path.append('/home/mario/dev/pythonHacks/django-sample/myenv/lib/python3.6/site-packages')
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mariopage.settings')
