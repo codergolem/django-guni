8bc863f7ab5843e2655a140be248591bd4f24bfa
codergolem <mariocast@posteo.de>
2019-12-18 21:08:00 -0600

    PR test

--- mariopage/polls/views.py
+++ mariopage/polls/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hi there, here is Mario again")
+    return HttpResponse("Hi there, here is Mario again - PR test")
